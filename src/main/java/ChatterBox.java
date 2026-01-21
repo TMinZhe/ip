5a4549fb37a2df84b6d7edb73fffb795c1c86ebe
TMinZhe <minzhetan@gmail.com>
2026-01-21 23:23:00 +0800

    Rename, Greet, Exit

---
+++ src/main/java/ChatterBox.java
@@ -0,0 +1,8 @@
+public class ChatterBox {
+    public static void main(String[] args) {
+        String botName = "ChatterBox";
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
