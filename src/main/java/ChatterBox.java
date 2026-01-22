1e9d9e52db47d309428777cbf170fdd403d84852
TMinZhe <minzhetan@gmail.com>
2026-01-22 15:49:25 +0800

    Echo

--- src/main/java/ChatterBox.java
+++ src/main/java/ChatterBox.java
@@ -1,8 +1,18 @@
 public class ChatterBox {
     public static void main(String[] args) {
         String botName = "ChatterBox";
-        System.out.println("Hello! I'm " + botName);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        boolean chatting = true;
+        System.out.println("        Hello! I'm " + botName);
+        System.out.println("        What can I do for you?");
+        while (chatting) {
+            String userInput = System.console().readLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                chatting = false;
+                break;
+            } else {
+                System.out.println("        " + userInput);
+            }
+        }
+        System.out.println("        Bye. Hope to see you again soon!");
     }
 }
